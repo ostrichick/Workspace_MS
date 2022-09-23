6f19cf9aca3d30c80f1fdd0f8bd67daca524c79a
ostrichick <95114705+ostrichick@users.noreply.github.com>
2022-09-23 17:45:50 +0900

    소나린트

--- workspace/.sonarlint/default/.sonartmp_7830006512247454860/eslint-bridge-bundle/package/lib/rules/decorators/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { decorateAccessorPairs } from './accessor-pairs-decorator';
-export declare const externalRuleDecorators: {
-    decorate: typeof decorateAccessorPairs;
-    ruleKey: string;
-}[];
