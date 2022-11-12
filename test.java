a03f31b034480ddade7e8ebcddf4f29b82eca565
ostrichick <gip4kun@gmail.com>
2022-11-12 21:25:40 +0900

    new

--- test.java
+++ test.java
@@ -2,9 +2,15 @@ class test {
 
   public static void main(String[] args) {
     System.out.println("hello world");
-    String FuncB(){
-    String x = null;
-    x = "Example";
-    return x;}
   }
+}
+
+class weather {
+
+  int 기온;
+  int 습도;
+  int 풍량;
+  int 강수량;
+  int 미세먼지;
+  String 날씨;
 }
