9318488ea364c3b629828e18d88822e7900ce9ff
ostrichick <gip4kun@gmail.com>
2023-04-16 18:12:12 +0900

    c 과제

---
+++ assignment/ex1-2.c
@@ -0,0 +1,19 @@
+/* 사용자로부터 정수 두 개를 입력받아 곱한 결과를 출력하는 프로그램 */
+#include <stdio.h> // 표준 입출력 헤더파일 추가
+#pragma warning(disable:4996) // 보안 경고를 무시
+int a, b, c; // 사용할 변수 정수로 선언
+int product(int x, int y); // 함수 원형 선언
+void main() // 메인 함수 시작
+{    
+    printf("Enter a number between 1 and 100: "); // 사용자에게 보여줄 메세지 출력
+    scanf("%d", &a); // 첫번째 수를 입력받고 값을 a에 저장
+    printf("Enter another number between 1 and 100: "); // 사용자에게 보여줄 메세지 출력
+    scanf("%d", &b); // 두번째 수를 입력받고 값을 b에 저장
+    c = product(a, b); // product() 함수 호출하여 c에 저장
+    printf("%d * %d = %d \n", a, b, c); // 저장한 결과 수식 형태로 출력
+} // 메인 함수 끝
+int product(int x, int y) // 두 수의 곱을 구하고 반환하는 함수
+{
+    return x * y;
+}
+
---
+++ assignment/ex2-1.c
@@ -0,0 +1,35 @@
+// c언어의 자료형에 대한 크기를 나타내는 프로그램
+// Path: assignment\ex2-2.c
+#include <stdio.h> // 표준 입출력 헤더파일 추가
+void main() // 메인 함수 시작
+{
+    printf("char 자료형 크기 : %d byte byte\n", sizeof(char)); // char 자료형의 크기 출력
+    printf("signed char 자료형 크기 : %d byte\n", sizeof(signed char)); // signed char 자료형의 크기 출력
+    printf("unsigned char 자료형 크기 : %d byte\n", sizeof(unsigned char)); // unsigned char 자료형의 크기 출력
+    printf("short 자료형 크기 : %d byte\n", sizeof(short)); // short 자료형의 크기 출력
+    printf("short int 자료형 크기 : %d byte\n", sizeof(short int)); // short int 자료형의 크기 출력
+    printf("signed short 자료형 크기 : %d byte\n", sizeof(signed short)); // signed short 자료형의 크기 출력
+    printf("signed short int 자료형 크기 : %d byte\n", sizeof(signed short int)); // signed short int 자료형의 크기 출력
+    printf("unsigned short 자료형 크기 : %d byte\n", sizeof(unsigned short)); // unsigned short 자료형의 크기 출력
+    printf("unsigned short int 자료형 크기 : %d byte\n", sizeof(unsigned short int)); // unsigned short int 자료형의 크기 출력
+    printf("int 자료형 크기 : %d byte\n", sizeof(int)); // int 자료형의 크기 출력
+    printf("signed int 자료형 크기 : %d byte\n", sizeof(signed int)); // signed int 자료형의 크기 출력
+    printf("unsigned 자료형 크기 : %d byte\n", sizeof(unsigned)); // unsigned 자료형의 크기 출력
+    printf("unsigned int 자료형 크기 : %d byte\n", sizeof(unsigned int)); // unsigned int 자료형의 크기 출력
+    printf("long 자료형 크기 : %d byte\n", sizeof(long)); // long 자료형의 크기 출력
+    printf("long int 자료형 크기 : %d byte\n", sizeof(long int)); // long int 자료형의 크기 출력
+    printf("signed long 자료형 크기 : %d byte\n", sizeof(signed long)); // signed long 자료형의 크기 출력
+    printf("signed long int 자료형 크기 : %d byte\n", sizeof(signed long int)); // signed long int 자료형의 크기 출력
+    printf("unsigned long 자료형 크기 : %d byte\n", sizeof(unsigned long)); // unsigned long 자료형의 크기 출력
+    printf("unsigned long int 자료형 크기 : %d byte\n", sizeof(unsigned long int)); // unsigned long int 자료형의 크기 출력
+    printf("long long 자료형 크기 : %d byte\n", sizeof(long long)); // long long 자료형의 크기 출력
+    printf("long long int 자료형 크기 : %d byte\n", sizeof(long long int)); // long long int 자료형의 크기 출력
+    printf("signed long long 자료형 크기 : %d byte\n", sizeof(signed long long)); // signed long long 자료형의 크기 출력
+    printf("signed long long int 자료형 크기 : %d byte\n", sizeof(signed long long int)); // signed long long int 자료형의 크기 출력
+    printf("unsigned long long 자료형 크기 : %d byte\n", sizeof(unsigned long long)); // unsigned long long 자료형의 크기 출력
+    printf("unsigned long long int 자료형 크기 : %d byte\n", sizeof(unsigned long long int)); // unsigned long long int 자료형의 크기 출력
+    printf("float 자료형 크기 : %d byte\n", sizeof(float)); // float 자료형의 크기 출력
+    printf("double 자료형 크기 : %d byte\n", sizeof(double)); // double 자료형의 크기 출력
+    printf("long double 자료형 크기 : %d byte\n", sizeof(long double)); // long double 자료형의 크기 출력
+} // 메인 함수 끝
+
